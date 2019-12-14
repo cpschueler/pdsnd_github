d162f8031ff85ae7171308e31cc8e1a2d0ae3fae
christopher schueler <cschueler@christophers-MacBook-Pro.local>
2019-12-14 13:34:24 +0300

    Add comment

--- my_bikeshare.py
+++ my_bikeshare.py
@@ -2,6 +2,10 @@ import time
 import pandas as pd
 import numpy as np
 
+'''
+Those things were imported!
+'''
+
 CITY_DATA = { 'chicago': 'chicago.csv',
               'new york city': 'new_york_city.csv',
               'washington': 'washington.csv' }
